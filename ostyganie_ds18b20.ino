413c959870975300c1f172136237534b4db8d23e
KacperMisztal <69186452+KacperMisztal@users.noreply.github.com>
2020-11-02 12:04:40 +0100

    Add files via upload
    Program dla Arduino odpowiedzialny za odczyt danych z czujników DS18B20, pomiar czasu i komunikację z komputerem.

---
+++ ostyganie_ds18b20.ino
@@ -0,0 +1,74 @@
+// Kod bazuje na przykładach dołącznonych do bibiliotek OneWire i DallasTemperature
+// Code based on examples included in OneWire and DallasTemperature libraries
+
+//Autor : Kacper Misztal
+
+// Biblioteki
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+// Ustawienia
+#define ONE_WIRE_BUS 10 //pin interfejsu OneWire
+#define TEMPERATURE_PRECISION 12 //rozdzielczość czujnika [bit], min = 9, max = 12
+long delayTime = 10000L;  //odstęp pomiędzy pomiarami
+long baudrate = 115200;   //Szybkość kounikacji, bit/s
+
+// Zmienne globalne
+unsigned int startTime; //czas działania programu
+
+// Przypisanie magistrali OneWire do odpowiedniego pinu
+OneWire oneWire(ONE_WIRE_BUS);
+
+// przekazanie danych OneWire do DallasTemperature
+DallasTemperature sensors(&oneWire);
+
+//Przypisanie adresów czujników
+DeviceAddress tempSensor0 = { 0x28, 0xE5, 0x35, 0x9F, 0x0A, 0x00, 0x00, 0x9B };
+DeviceAddress tempSensor1 = { 0x28, 0x9A, 0x6B, 0x9B, 0x0A, 0x00, 0x00, 0x1E };
+DeviceAddress tempSensor2 = { 0x28, 0x2B, 0x40, 0x99, 0x0A, 0x00, 0x00, 0xA1 };
+
+//funkcja odpowiedzialna za pomiar czasu
+String getFormattedTime(){
+  unsigned long timeInMs = millis() - startTime;  //obliczenie aktualnego czasu w ms
+  unsigned int timeS = timeInMs / 1000;
+  unsigned int timeM = timeInMs / 60000;
+  unsigned int timeH = timeInMs / 3600000;
+  return String(timeH) + ":" + String(timeM % 60) + ":" + String(timeS % 60);
+}
+
+void setup(void)
+{
+  // Komunikacja z komputerem
+  Serial.begin(baudrate); //Rozpoczęcie komunikacji przez port szeregowy
+  while(!Serial); //Oczekiwanie na połączenie
+  Serial.println("Nawiązano połączenie\nCzas, T1, T2, T3");  //komunikat o nawiązaniu połączenia
+  //zapisanie czasu
+  startTime = millis(); 
+
+  // rozpoczecie używania czjuników
+  sensors.begin();
+
+  // Szukanie urządzeń OneWire
+  Serial.print("Szukanie czujników... ");
+  Serial.print(sensors.getDeviceCount(), DEC);
+  Serial.println(" czujniki.");
+
+  // ustawienie zadanej wcześniej rozdzielczości
+  sensors.setResolution(tempSensor0, TEMPERATURE_PRECISION);
+  sensors.setResolution(tempSensor1, TEMPERATURE_PRECISION);
+  sensors.setResolution(tempSensor2, TEMPERATURE_PRECISION);
+}
+
+//funkcja główna
+void loop(void)
+{
+  // pomiar czasu wykonania programu
+  unsigned long beginTime = millis();
+  
+  //wysłanie żądania do czujników
+  sensors.requestTemperatures();
+
+  // przesłanie wyników
+  Serial.println(getFormattedTime() + ", " + String(sensors.getTempC(tempSensor0)) + ", " + String(sensors.getTempC(tempSensor1)) + ", " + String(sensors.getTempC(tempSensor2)));
+  delay(delayTime - (millis() - beginTime));  // odczekanie zadanego czasu - czas wykonania programu
+}
